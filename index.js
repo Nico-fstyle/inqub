339bb1d0fee063f7de9555aa586245003f9de13c
El Khamlichi Idriss <idriss.el-khamlichi@student-cs.fr>
2024-02-24 10:50:21 +0100

    envoi du serveur socket vers le client de l'info utile

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { Server } = require("socket.io");
 
 const app = express();
+const LEN_LAYERS = 13
 
 const { createServer } = require('http');
 console.log('Lien vers le serveur express nodemon : %c http://localhost:5000/', 'color: blue; text-decoration: underline;');
@@ -18,15 +19,20 @@ function tick () {
         if (moveMap[player.id]) {
             player.x = moves.x;
             player.y = moves.y;
-        };
+
+            
+        }
+        if (encount[player.id]) {
+            player.encounters = encount[player.id];
+        }
+
     }
     io.emit('players', players);
-
-
 }
 
 const players = [];
 const moveMap = {};
+const encount = [];
 
 io.on('connect', (socket) => {
     console.log('user connected', socket.id);
@@ -34,27 +40,33 @@ io.on('connect', (socket) => {
         x: 664,
         y: 30
     };
+    encount[socket.id] = Array.from({ length: LEN_LAYERS }, () => -1);
     players.push({
         id: socket.id,
         x: 664,
-        y: 30
+        y: 30,
+        encounters: encount[socket.id],
     });
     
     socket.on('move', (movements) => {
         moveMap[socket.id] = movements;
-        console.log(movements);
+        console.log(moveMap);
 
           });
 
     socket.on('encounter', (data) => {
-        console.log(data.layer);
     if (data.layer[1] === 1) {
             console.log(`player ${socket.id} entered layer ${data.layer[0]}`);
+            encount[socket.id][data.layer[0]] = 1;
         }
         else {
             console.log(`player ${socket.id} exited layer ${data.layer[0]}`);
+            encount[socket.id][data.layer[0]] = -1;
         };
-    })
+    });
+
+    
+
 });
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
